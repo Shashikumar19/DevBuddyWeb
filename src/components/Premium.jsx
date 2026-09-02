c540b66025fa340deef731fcb7219aa52612a0c7
shashikumar <rathodshashikumar72@gmail.com>
2026-09-02 22:34:18 +0530

    updated styles

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -53,12 +53,14 @@ const Premium = () => {
         }
     }
 
-    if (isPremium) return <div role="alert" className="alert alert-warning">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-        </svg>
-        <span>you are an premium member</span>
-    </div>
+    if (isPremium) return (<div className="flex justify-center mt-5">
+        <div role="alert" className="alert alert-warning w-2xl">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+            </svg>
+            <span>you are an premium member</span>
+        </div>
+    </div>)
 
     return <div>
         <div className="flex w-full p-10">
